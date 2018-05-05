52c82785b61d1ca7366fbe7ea36fd0e0db3f3a7d
Tolga Kaprol <tolgakaprol@gmail.com>
2018-05-05 18:43:25 +0300

    Meetup Demo

--- meetup-istanbul.php
+++ meetup-istanbul.php
@@ -10,4 +10,20 @@ License: GPLv2 or later
 Text Domain: meetup-istanbul
 */
 
+function meetup_istanbul_the_content($output)
+{
+
+  $output .= '<div style="border:1px solid red;">Bu Icerik Kaynak Gosterilmeden Kullanilamaz</div>';
+
+  return $output;
+}
+
+add_filter( 'the_content' , 'meetup_istanbul_the_content' );
+
+function meetup_istanbul_header() {
+  echo '<!-- HERE -->';
+}
+
+add_action( 'wp_head' , 'meetup_istanbul_header' );
+
 ?>
