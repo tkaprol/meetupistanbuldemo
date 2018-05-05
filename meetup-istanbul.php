370223331db275acc43c747756119f94d37d0b21
Tolga Kaprol <tolgakaprol@gmail.com>
2018-05-05 11:54:18 +0300

    Initial Commit

---
+++ meetup-istanbul.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Meetup Istanbul
+Plugin URI: http://etom.com.tr/meetup-istanbul
+Description: Meetup Plugin Gelistirme Ornegi
+Version: 0.0.1
+Author: ETOM
+Author URI: http://etom.com.tr
+License: GPLv2 or later
+Text Domain: meetup-istanbul
+*/
+
+?>
